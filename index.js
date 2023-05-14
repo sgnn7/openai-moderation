3ff82bb7c4ea2604506a082233288819ebd31ccc
Srdjan Grubor <sgnn7@sgnn7.org>
2023-05-14 17:00:19 -0500

    [index] Add canned skeleton to the main index file
    We need something to start running workflows against and to test.

---
+++ index.js
@@ -0,0 +1,22 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  const apiKeySanitizationPattern = /.*/
+  // `openai-api-key` input defined in action metadata file
+  const openAIApiKey = core.getInput('openai-api-key');
+
+  // TODO: Remove me
+  console.log(`API Key: ${openAIApiKey.replace(pattern, "X"}!`);
+
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+
+  /*
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+  */
+} catch (error) {
+  core.setFailed(error.message);
+}
